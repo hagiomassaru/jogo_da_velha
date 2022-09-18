01e0d2c82686340f35b5eb3a7658db2e03574ab6
Massaru <massaruhagiopro@gmail.com>
2022-09-18 23:59:15 +0900

    criando condicao de vitoria

--- scripts/game.js
+++ scripts/game.js
@@ -2,14 +2,34 @@
 let board = ["", "", "", "", "", "", "", "", ""];
 let playTime = 0;
 let simbols = ["x", "o"];
+let gameOver = false;
 
 function handMove(position) {
+    if (gameOver) {
+        return;
+    }
     if (board[position] == "") {
         board[position] = simbols[playTime];
+
+        gameOver = isWin();
+
         if (playTime == 0) {
             playTime = 1;
         } else {
             playTime = 0;
         }
     }
+}
+
+function isWin() {
+    let winstate = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
 }
